f08c078217b3cba95d923d6ab57edc0dbc27f60e
xuewei <isxuewei@qq.com>
2023-11-08 10:46:26 +0800

    feat: 初始化项目

--- xw-fast-all/src/main/java/world/xuewei/fast/all/HelloWorld.java
+++ xw-fast-all/src/main/java/world/xuewei/fast/all/HelloWorld.java
@@ -5,6 +5,16 @@ package world.xuewei.fast.all;
  * @since 2023/11/7 21:37
  */
 public class HelloWorld {
+
+    static {
+        System.out.println("\n" +
+                "   _  ___       __      _________   ___________\n" +
+                "  | |/ / |     / /     / ____/   | / ___/_  __/\n" +
+                "  |   /| | /| / /_____/ /_  / /| | \\__ \\ / /   \n" +
+                " /   | | |/ |/ /_____/ __/ / ___ |___/ // /    \n" +
+                "/_/|_| |__/|__/     /_/   /_/  |_/____//_/     \n");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
