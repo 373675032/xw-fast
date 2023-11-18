7b65c76bc69f1d896d89e3b8bbcd3e8d4609921a
xuewei <isxuewei@qq.com>
2023-11-18 12:35:06 +0800

    feat(crud): 引入 MyBatisPlus 分页插件

---
+++ xw-fast-crud/src/main/java/world/xuewei/fast/crud/config/MyBatisPlusConfig.java
@@ -0,0 +1,31 @@
+package world.xuewei.fast.crud.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatisPlus 配置类
+ *
+ * @author XUEW
+ * @since 2023/11/18 12:15
+ */
+@Configuration
+public class MyBatisPlusConfig {
+
+    /**
+     * MybatisPlus 拦截器配置插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 向 Mybatis 过滤器链中添加分页拦截器
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 添加乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+    }
+}
